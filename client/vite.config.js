58187ee4e95ee21ffefafa08451742b42b75db00
Shaurya699 <raishaurya50@gmail.com>
2024-10-19 18:23:29 +0530

    changes in vite comfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Output directory for build files
-    assetsDir: "assets", // Directory for static assets (js, css, etc.)
-    emptyOutDir: true, // Ensure the output directory is emptied before building
-    rollupOptions: {
-      // Customize build options if necessary, like input/output
-    },
-  },
-  server: {
-    port: 3000, // Set the development server port if needed
-    open: true, // Automatically open the app in the browser on dev server start
-  },
-  resolve: {
-    alias: {
-      // You can add path aliases here if needed, for example:
-      "@": "/src",
-    },
+    outDir: "../backend/dist", // Output build files to backend/dist
+    emptyOutDir: true,
+    assetsDir: "assets",
   },
 });
